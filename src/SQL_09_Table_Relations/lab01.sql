9f10f5d7b85fe3dcfa84a2008d8a3a3d87a80269
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-05 16:09:55 +0300

    sql exercise

---
+++ src/SQL_09_Table_Relations/lab01.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE camp.`mountains` (
+`id` INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(45) NOT NULL);
+CREATE TABLE camp.`peaks` (
+id INT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(45) NOT NULL,
+mountain_id INT not null ,
+CONSTRAINT fk_mountain_id
+FOREIGN KEY (mountain_id)
+REFERENCES `mountains`(id) ON UPDATE CASCADE
+);

a7fcddae163000c244746bcb7024962707c67686
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-05 16:13:17 +0300

    sql exercise

--- src/SQL_09_Table_Relations/lab01.sql
+++ src/SQL_09_Table_Relations/lab01.sql
@@ -8,7 +8,7 @@ CREATE TABLE camp.`peaks`
 (
     id          INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name`      VARCHAR(45)     NOT NULL,
-    mountain_id INT             not null,
+    mountain_id INT             NOT NULL,
     CONSTRAINT fk_mountain_id
         FOREIGN KEY (mountain_id)
             REFERENCES `mountains` (id) ON UPDATE CASCADE

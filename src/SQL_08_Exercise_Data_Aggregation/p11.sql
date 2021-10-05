02b3f865b4bbbac1e12e97cabba844da2aff8ead
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-05 05:42:25 +0300

    sql exercise data aggregation

--- src/SQL_08_Exercise_Data_Aggregation/p11.sql
+++ src/SQL_08_Exercise_Data_Aggregation/p11.sql
@@ -4,5 +4,5 @@ AVG(deposit_interest) AS 'average_interest'
 FROM wizzard_deposits
 WHERE deposit_start_date > '1985-01-01'
 GROUP BY deposit_group,is_deposit_expired
-ORDER BY deposit_group DESC , is_deposit_expired;
+ORDER BY deposit_group DESC , is_deposit_expired
 

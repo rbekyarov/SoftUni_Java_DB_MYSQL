c43c609a2db07dfde4d81d820af9914ebb1e76c8
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-09 19:57:35 +0300

    SQL Exercise Joins

---
+++ src/SQL_12_Exercise_Subqueries_and_JOINs/p01.sql
@@ -0,0 +1,6 @@
+USE soft_uni;
+SELECT employee_id,	job_title,	a.address_id,	address_text
+FROM employees
+JOIN addresses a on employees.address_id = a.address_id
+ORDER BY address_id
+LIMIT 5;
---
+++ src/SQL_12_Exercise_Subqueries_and_JOINs/p02.sql
@@ -0,0 +1,7 @@
+USE soft_uni;
+SELECT e.first_name,	e.last_name,	t.name AS town,	a.address_text
+FROM employees AS e
+JOIN addresses AS a on a.address_id = e.address_id
+JOIN towns AS t on t.town_id = a.town_id
+ORDER BY first_name, last_name
+LIMIT 5;

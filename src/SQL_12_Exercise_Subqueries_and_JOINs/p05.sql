87697227f73475a7669f418c2a1f2f562306d9e8
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-09 20:27:45 +0300

    SQL Exercise Joins

---
+++ src/SQL_12_Exercise_Subqueries_and_JOINs/p05.sql
@@ -0,0 +1,9 @@
+USE soft_uni;
+SELECT e.employee_id, e.first_name
+FROM employees AS e
+LEFT JOIN  employees_projects AS ep
+ON e.employee_id = ep.employee_id
+LEFT JOIN   projects AS p
+ON ep.project_id = p.project_id
+WHERE p.name IS NULL
+ORDER BY e.employee_id DESC;

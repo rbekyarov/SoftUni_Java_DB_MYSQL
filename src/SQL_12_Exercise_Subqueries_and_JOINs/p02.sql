510a4c7740a9b394f92863c452c069d9b3529a96
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-09 19:58:34 +0300

    SQL Exercise Joins

--- src/SQL_12_Exercise_Subqueries_and_JOINs/p02.sql
+++ src/SQL_12_Exercise_Subqueries_and_JOINs/p02.sql
@@ -1,7 +1,7 @@
 USE soft_uni;
 SELECT e.first_name,	e.last_name,	t.name AS town,	a.address_text
 FROM employees AS e
-JOIN addresses AS a on a.address_id = e.address_id
-JOIN towns AS t on t.town_id = a.town_id
+JOIN addresses AS a ON a.address_id = e.address_id
+JOIN towns AS t ON t.town_id = a.town_id
 ORDER BY first_name, last_name
 LIMIT 5;

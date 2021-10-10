86c82fcbb437321578a7ff8ffe1904039400aab4
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-10-10 07:39:24 +0300

    SQL Exercise Joins

---
+++ src/SQL_12_Exercise_Subqueries_and_JOINs/p12.sql
@@ -0,0 +1,11 @@
+USE geography;
+SELECT c.country_code, m.mountain_range, p.peak_name, p.elevation
+FROM countries AS c
+JOIN mountains_countries AS mc
+USING (country_code)
+JOIN mountains AS m
+ON m.id = mc.mountain_id
+JOIN peaks AS p
+ON p.mountain_id = m.id
+WHERE c.country_name = 'Bulgaria' AND p.elevation > 2835
+ORDER BY p.elevation DESC ;

548061385a740f0c2625202a3dfb5dc97868ba6a
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-09-26 20:14:52 +0300

    Update p15.sql

--- src/SQL_08_Exercise_Data_Aggregation/p15.sql
+++ src/SQL_08_Exercise_Data_Aggregation/p15.sql
@@ -1,6 +1,3 @@
-USE soft_uni;
-SELECT department_id, MAX(salary) AS 'max_salary'
-FROM employees
-GROUP BY department_id
-HAVING max_salary NOT BETWEEN 30000 AND 70000
-ORDER BY department_id;
+use soft_uni;
+select COUNT(*) from employees
+where manager_id is NULL

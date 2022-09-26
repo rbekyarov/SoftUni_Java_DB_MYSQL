ce0bfb536a8caf0c50f80e596648098564687866
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-09-26 20:15:18 +0300

    Update p16.sql

--- src/SQL_08_Exercise_Data_Aggregation/p16.sql
+++ src/SQL_08_Exercise_Data_Aggregation/p16.sql
@@ -1,5 +1,11 @@
-USE soft_uni;
-SELECT COUNT(salary) AS '' FROM employees
-WHERE manager_id IS NULL
+use soft_uni;
+select DISTINCT department_id , (
+    SELECT DISTINCT salary
+    FROM    `employees` e
+    WHERE   e.`department_id` = `employees`.`department_id`
+    order by salary DESC
+    LIMIT 1 offset 2) as `third_highest_salary`
 
-
+from employees
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
